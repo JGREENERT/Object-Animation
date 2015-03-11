f4ecb16641971342f93c21204f906778533488fb
unknown <greenerj@mail.gvsu.edu>
2015-03-10 22:57:58 -0400

    Finished the Merry Go Round Object.

--- Complex Objects/MerryGoRound.cpp
+++ Complex Objects/MerryGoRound.cpp
@@ -17,7 +17,7 @@ void MerryGoRound::build()
 {
     base.build(1.0f, 1.0f, 8.0f);
     handleBase.build(0.25f, 0.25f, 1.5f);
-    handle.build(3, 10, 5, 5);
+    handle.build(1.5f, 0.25f, 10, 10, 180);
     gluQuadricOrientation(d, GLU_OUTSIDE);
     gluQuadricNormals(d, GLU_SMOOTH);
 }
@@ -34,7 +34,7 @@ void MerryGoRound::render() const
     glColor3ub (255, 0, 0);
     glPushMatrix();
     glTranslated(0, 0, 8);
-    gluDisk(d, 0, 10, 100, 1);
+    gluDisk(d, 0, 10, 100, 5);
     glPopMatrix();
 
     int rotation = 0;
@@ -57,14 +57,17 @@ void MerryGoRound::render() const
         handleBase.render();
         glPopMatrix();
 
+        glColor3ub (114, 103, 103);
+        glPushMatrix();
+        glTranslated(0, 3.5, 14.75);
+        glRotated(90, 0, 0, 1);
+        glRotated(90, 1, 0, 0);
+        handle.render();
+        glPopMatrix();
+
         rotation += 90;
     }
     glPopMatrix();
     glPopMatrix();
-
-    glPushMatrix();
-    handle.render();
-    glPopMatrix();
-
     glPopMatrix();
 }
